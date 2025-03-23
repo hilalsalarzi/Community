c2b191145d69854647a4c719875359ddee95f744
hilalsalarzi <hilalahmadsalarzi@gmail.com>
2025-03-24 00:09:28 +0500

    layout

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="bg-gray-800 text-white p-4 shadow-md">
+        <h1 className="text-xl font-bold">Admin Panel</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
